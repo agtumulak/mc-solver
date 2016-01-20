71289c9e8a24514f579c3bf368146d7adf409493
Aaron Tumulak <agtumulak@gmail.com>
2016-01-20 17:12:55 -0500

    Added Segment class

---
+++ src/segment.cpp
@@ -0,0 +1,28 @@
+// segment.cpp
+// Aaron G. Tumulak
+
+// std includes
+#include <iostream>
+
+// mc-solver includes
+#include "material.hpp"
+#include "segment.hpp"
+
+// Default constructor
+Segment::Segment( Material material, double width, int num_cells ):
+    material_( material ),
+    width_( width ),
+    num_cells_( num_cells )
+{}
+
+// Friend functions //
+
+// Overload operator<<()
+std::ostream &operator<< ( std::ostream &out, const Segment &obj )
+{
+    out << "Width: \t" << obj.width_ << std::endl;
+    out << "Number of cells: " << obj.num_cells_ << std::endl;
+    out << "Material: \n\n" << obj.material_ << std::endl;
+
+    return out;
+}
---
+++ src/segment.hpp
@@ -0,0 +1,38 @@
+// segment.hpp
+// Aaron G. Tumulak
+
+#pragma once
+
+// std includes
+#include <iostream>
+#include <vector>
+
+// mc-solver includes
+#include "material.hpp"
+
+class Segment
+{
+    public:
+
+        // Default constructor
+        Segment( Material material, double width, int num_cells );
+
+        // Overload operator<<()
+        friend std::ostream &operator<< ( std::ostream &out, const Segment &obj );
+
+    private:
+
+        // Material
+        const Material material_;
+
+        // Width
+        const double width_;
+
+        // Number of equallu spaced cells
+        const int num_cells_;
+};
+
+// Friend functions //
+
+// Overload operator<<()
+std::ostream &operator<< ( std::ostream &out, const Segment &obj );
