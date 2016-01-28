804865cd1e9f0b1a1af0680769d6b2fb770fbd03
Aaron Tumulak <agtumulak@gmail.com>
2016-01-27 23:49:12 -0500

    Updated mc-solver to test purely absorbing slab

--- src/mc-solver.cpp
+++ src/mc-solver.cpp
@@ -17,30 +17,31 @@ int main()
     
     Settings settings_1;
     settings_1.WriteSeed( 10 );
-    settings_1.WriteHistories( 100 );
+    settings_1.WriteHistories( 1000000 );
 
     // Create material mat_1 //
     Material mat_1;
 
     // Absorption
-    mat_1.WriteMacroAbsXsec( 0.1, 1.0 );
+    mat_1.WriteMacroAbsXsec( 0.1, 0.1 );
 
     // Scattering
-    mat_1.WriteMacroScatXsec( 0.1, 0.1, 1.0 );
+    mat_1.WriteMacroScatXsec( 0.1, 0.1, 0.0 );
     mat_1.WriteMacroScatXsec( 0.1, 14.0, 0.0 );
-    mat_1.WriteMacroScatXsec( 14.0, 0.1, 1.0 );
+    mat_1.WriteMacroScatXsec( 14.0, 0.1, 0.0 );
     mat_1.WriteMacroScatXsec( 14.0, 14.0, 0.0 );
 
     // Fission
-    mat_1.WriteMacroFissXsec( 0.1, 1.0 );
+    mat_1.WriteMacroFissXsec( 0.1, 0.0 );
     mat_1.WriteFissNu( 1.0 );
     mat_1.WriteFissChi( 0.1, 0.0 );
     mat_1.WriteFissChi( 14.0, 1.0 );
 
     // External source
     mat_1.WriteExtSource( 0.1, 1.0 );
-    mat_1.WriteExtSource( 14.0, 1.0 );
+    mat_1.WriteExtSource( 14.0, 0.0 );
 
+    /*
     // Create material mat_2 //
     Material mat_2;
 
@@ -62,19 +63,16 @@ int main()
     // External source
     mat_2.WriteExtSource( 0.1, 2.0 );
     mat_2.WriteExtSource( 14.0, 2.0 );
+    */
 
     // Create layout //
     Layout layout_1;
-    layout_1.AddToEnd( mat_1, 100.0, 2 );
-    layout_1.AddToEnd( mat_2, 50.0, 2 );
-    layout_1.AddToEnd( mat_1, 400.0, 1 );
+    layout_1.AddToEnd( mat_1, 100.0, 400 );
 
     // Create slab
     Slab slab_1( settings_1, layout_1 );
 
     // Test
-    std::cout << slab_1 << std::endl;
-
     slab_1.RunHistories();
     slab_1.ListTrackLengthEstimators( 0.1 );
     
