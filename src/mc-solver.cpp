31cc149634d96c4a000a14fb34a7d027952f9d22
Aaron Tumulak <agtumulak@gmail.com>
2016-02-05 13:19:53 -0500

    Testing repeating slab problem

--- src/mc-solver.cpp
+++ src/mc-solver.cpp
@@ -17,58 +17,71 @@ int main()
     
     Settings settings_1;
     settings_1.WriteSeed( 10 );
-    settings_1.WriteHistories( 100000000 );
-    settings_1.WriteProgressPeriod( 100000 );
+    settings_1.WriteHistories( 10000000 );
+    settings_1.WriteProgressPeriod( 10000 );
 
-    // Create material mat_1 //
+    // Create moderator material mat_1 //
     Material mat_1;
 
     // Absorption
     mat_1.WriteMacroAbsXsec( 0.1, 0.05 );
+    mat_1.WriteMacroAbsXsec( 14.0, 0.0 );
 
     // Scattering
     mat_1.WriteMacroScatXsec( 0.1, 0.1, 0.05 );
     mat_1.WriteMacroScatXsec( 0.1, 14.0, 0.0 );
-    mat_1.WriteMacroScatXsec( 14.0, 0.1, 0.0 );
+    mat_1.WriteMacroScatXsec( 14.0, 0.1, 0.1 );
     mat_1.WriteMacroScatXsec( 14.0, 14.0, 0.0 );
 
     // Fission
     mat_1.WriteMacroFissXsec( 0.1, 0.0 );
+    mat_1.WriteMacroFissXsec( 14.0, 0.0 );
     mat_1.WriteFissNu( 1.0 );
     mat_1.WriteFissChi( 0.1, 0.0 );
     mat_1.WriteFissChi( 14.0, 1.0 );
 
     // External source
-    mat_1.WriteExtSource( 0.1, 1.0 );
+    mat_1.WriteExtSource( 0.1, 0.1 );
     mat_1.WriteExtSource( 14.0, 0.0 );
 
-    /*
-    // Create material mat_2 //
+    // Create fissile material mat_2 //
     Material mat_2;
 
     // Absorption
-    mat_2.WriteMacroAbsXsec( 0.1, 2.0 );
+    mat_2.WriteMacroAbsXsec( 0.1, 0.0 );
+    mat_2.WriteMacroAbsXsec( 14.0, 0.1 );
 
     // Scattering
-    mat_2.WriteMacroScatXsec( 0.1, 0.1, 2.0 );
+    mat_2.WriteMacroScatXsec( 0.1, 0.1, 0.05 );
     mat_2.WriteMacroScatXsec( 0.1, 14.0, 0.0 );
-    mat_2.WriteMacroScatXsec( 14.0, 0.1, 2.0 );
+    mat_2.WriteMacroScatXsec( 14.0, 0.1, 0.0 );
     mat_2.WriteMacroScatXsec( 14.0, 14.0, 0.0 );
 
     // Fission
-    mat_2.WriteMacroFissXsec( 0.1, 2.0 );
+    mat_2.WriteMacroFissXsec( 0.1, 0.05 );
+    mat_2.WriteMacroFissXsec( 14.0, 0.0 );
     mat_2.WriteFissNu( 1.0 );
     mat_2.WriteFissChi( 0.1, 0.0 );
     mat_2.WriteFissChi( 14.0, 1.0 );
 
     // External source
-    mat_2.WriteExtSource( 0.1, 2.0 );
-    mat_2.WriteExtSource( 14.0, 2.0 );
-    */
+    mat_2.WriteExtSource( 0.1, 0.1 );
+    mat_2.WriteExtSource( 14.0, 0.0 );
 
     // Create layout //
     Layout layout_1;
-    layout_1.AddToEnd( mat_1, 100.0, 400 );
+    layout_1.AddToEnd( mat_1, 50.0, 500 );
+    layout_1.AddToEnd( mat_2, 50.0, 500 );
+    layout_1.AddToEnd( mat_1, 50.0, 500 );
+    layout_1.AddToEnd( mat_2, 50.0, 500 );
+    layout_1.AddToEnd( mat_1, 50.0, 500 );
+    layout_1.AddToEnd( mat_2, 50.0, 500 );
+    layout_1.AddToEnd( mat_1, 50.0, 500 );
+    layout_1.AddToEnd( mat_2, 50.0, 500 );
+    layout_1.AddToEnd( mat_1, 50.0, 500 );
+    layout_1.AddToEnd( mat_2, 50.0, 500 );
+    layout_1.AddToEnd( mat_1, 50.0, 500 );
+    layout_1.AddToEnd( mat_2, 25.0, 250 );
 
     // Create slab
     Slab slab_1( settings_1, layout_1 );
@@ -76,6 +89,7 @@ int main()
     // Test
     slab_1.RunHistories();
     slab_1.ListTrackLengthEstimators( 0.1 );
+    slab_1.ListTrackLengthEstimators( 14.0 );
     
     return 0;
 }
